a2bc7e4e7cab52e997360968b031e225cd8bac01
JGCan <jhgcandia@gmail.com>
2021-04-26 23:16:11 -0400

    ready for heroku

--- server.js
+++ server.js
@@ -6,5 +6,3 @@ app.use(express.static('./dist/portafolio-web'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'/dist/portafolio-web/index.html'));
 })
- 
-app.listen(3000)
