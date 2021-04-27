c1ea6e697eb0754ca426c0fb8a50d1d061ddde1d
JGCan <jhgcandia@gmail.com>
2021-04-27 14:00:36 -0400

    second deploy test

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
+//Install express server
+const express = require('express');
+const path = require('path');
 
+const app = express();
+
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/portafolio-web'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'/dist/portafolio-web/index.html'));
-})
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/portafolio-web/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
